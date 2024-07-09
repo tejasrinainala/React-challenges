0a931182370ecf69b065f34e6ae1cb1a52119c83
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-07-09 12:46:55 +0530

    Create Header.jsx

---
+++ simple-food-restaurant/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="button-container">
+                <button>Our Menu</button> 
+                <button>About</button> 
+                <button>Hours & Location</button>
+            </div> 
+        </header>   
+    ) 
+}
