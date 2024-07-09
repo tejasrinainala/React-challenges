8965f8a049506a5ac643671e692feb674a8f377c
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-07-09 12:47:44 +0530

    Create Footer.jsx

---
+++ simple-food-restaurant/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer>
+             <p> Made by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Cheeky Coding Company</a>™.
+             </p>
+        </footer>
+    )
+}
