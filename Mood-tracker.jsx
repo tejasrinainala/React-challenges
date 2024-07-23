0e66affcc377132df97ccfe6f8ea53a0ad1eabef
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-07-23 20:01:24 +0530

    Update Mood-tracker.jsx

--- Mood-tracker.jsx
+++ Mood-tracker.jsx
@@ -1,1 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import diaryData from "./diaryData"
+import DiaryCard from "./components/DiaryCard"
+import Menu from "./components/Menu"
 
+export default function App() {
+    
+/* Challenge 
+
+Two DiaryCard components are currently hardcoded with data below. Your task is to dynamically create DiaryCard components for all 14 days of data in diaryData.js. 
+
+    1. Both of the hardcoded DiaryCard components should be deleted and replaced with a total of 14 
+       dynmically-created components — one for each entry in diaryData.js. 
+    
+    2. The first two cards should look identical to the original hardcoded cards, and the rest of 
+       the cards should look similar. 
+        
+    3. Make the code concise. 
+*/
+    const diaryCardElements = diaryData.map( day => {
+        return (
+             <DiaryCard
+                key={day.id}
+                {...day}
+            />
+        )
+    })
+    
+    return (
+        <div>
+            <Menu skip={false}/>
+            <main className="main-container">
+                <div className="sub-header">
+                    <h3>April Reports</h3>
+                </div>
+                {diaryCardComponents}    
+            </main>
+        </div>
+    )
+}
